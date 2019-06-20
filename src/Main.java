847fa344d4c3787fca00124cb8cb3aeac46acbee
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2019-06-20 15:45:45 +1200

    Committed from eclipse

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         // Will this show up on the git?
         System.out.println("Hello World!");
+        
+        // Now I'm going to commit this from eclipse!
     }
 }
